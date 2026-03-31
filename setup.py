9077c550aa2f139f6213e5c15cde301fa1c78c8a
Ali <aliakbar357589@gmail.com>
2026-03-31 10:51:25 +0600

    logging and exception

--- setup.py
+++ setup.py
@@ -3,11 +3,15 @@ from typing import List
 
 def get_requirements(file_path:str)->List[str]:
     
+    HYPEN_E_DOT = '-e .'
     requirements = []
     with open(file_path) as file:
         requirements = file.readlines()
         requirements = [req.replace("\n", "") for req in requirements]
-
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+            
+    return requirements
 
 setup(
     name='MLOps project 01',
